012f69938b4ea48af4e40885f7e1607c92af883d
Nikita Ostrenkov <xfakehopex@gmail.com>
2021-06-03 15:49:25 +0300

    Initial commit Create empty iOS application from template

---
+++ CalcX/CalcXApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcXApp.swift
+//  CalcX
+//
+//  Created by Ostrenkov Nikita on 03.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct CalcXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
